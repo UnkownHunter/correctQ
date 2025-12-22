5ba95ef48a2e63b8fff90a3da62ad6e2e4139dd2
UnkownHunter <akashsannala2@gmail.com>
2025-12-23 00:33:06 +0530

    export for all components

---
+++ correctQ/frontend/src/components/History.jsx
@@ -0,0 +1,8 @@
+export default function History() {
+  return (
+    <>
+      <div>This is history component</div>
+      {/* this where the history of the past exams are displayed */}
+    </>
+  );
+}
---
+++ correctQ/frontend/src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <div>This is main component</div>
+      {/* this where the name of the exam and documents from the user are displayed*/}
+    </>
+  );
+}
